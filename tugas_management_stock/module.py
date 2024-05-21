b86a430c07e390cd2d2d88396118f4cdd907583c
Husain Mulyansyah <145633025+HusainD4@users.noreply.github.com>
2024-05-21 21:44:12 +0700

    first

---
+++ tugas_management_stock/module.py
@@ -0,0 +1,23 @@
+import manage as mg
+
+while True:
+    mg.menu()
+    pilihan = int(input('Masukan Pilihan : '))
+
+    if pilihan == 1:
+        mg.tambah()
+    if pilihan == 2:
+        mg.edit()
+    if pilihan == 3:
+        mg.delete()
+    if pilihan == 4:
+        mg.cari_data()
+    if pilihan == 5:
+        mg.data_barang()
+    if pilihan == 6:
+        mg.tampilkan_jumlah_data_barang()
+    if pilihan == 7:
+        print('='*25)  
+        print("TERIMAKASIH")  
+        print('='*25)
+        exit()  
